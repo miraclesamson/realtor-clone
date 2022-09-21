eb781dff0627fcb95ed6851724d8f3cd6a1433d8
Ogunmola Miracle Samson <106530260+miraclesamson@users.noreply.github.com>
2022-09-21 17:38:22 -0500

    Delete .history/src directory

--- .history/src/hooks/useAuthStatus_20220921014530.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useState } from "react";
-
-export default function useAuthStatus() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  return <div>useAuthStatus</div>;
-}
